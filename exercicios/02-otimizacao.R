7c27fc0b949d6828e8b6a4ce70e102c35b26ed40
Julio Trecenti <julio.trecenti@gmail.com>
2022-04-12 08:59:58 -0300

    Update 02-otimizacao.R

--- exercicios/02-otimizacao.R
+++ exercicios/02-otimizacao.R
@@ -4,9 +4,9 @@ library(dados)
 library(lubridate)
 library(ggthemes)
 
-# O objetivo dos exercícios abaixo é praticar conceitos de otimização do tema,
-# e escalas no ggplot2. 
-# Oferecemos um gráfico simples, e instruções de como otimizá-lo. 
+# O objetivo dos exercícios abaixo é praticar conceitos de otimização visual
+# usando temas, cores e escalas no ggplot2. 
+# Oferecemos um gráfico simples e instruções de como otimizá-lo. 
 # Para cada um deles, existe um arquivo .png com o mesmo completo,
 # para que você possa comparar/se inspirar.
 
@@ -26,26 +26,17 @@ pinguins |>
   ),
   alpha = 0.8) +
   # a....
-  ggthemes::theme_fivethirtyeight() +
   # b ...
-  scale_color_brewer(type = "qual", palette = 2) +
   # c ...
-  labs(
-    color = "Espécie",
-    shape = "Espécie",
-    x = "Massa corporal (g)",
-    y = "Comprimento do bico (cm)"
-  )
 
 
 # 2. Usando o gráfico a seguir, faça os exercícios:
 # a) adicione o tema `theme_solarized` usando o pacote ggthemes
 # b) adicione uma escala de cor viridis aos pontos
-# c) adicione um limite no eixo x: as notas podem
-# ser de 0 à 100
+# c) adicione um limite no eixo x: as notas podem ser de 0 a 100
 # d) adicione as legendas corretas com a função labs() .
 # e) usando a função theme(), deixe o tamanho do texto das legendas
-# dos eixos x e y com o tamanho 8, e o tamanho do texto  dos
+# dos eixos x e y com o tamanho 8, e o tamanho do texto dos
 # títulos das legendas com tamanho 10.
 # Exemplo de resultado: 02-pixar.png
 pixar_avalicao_publico |>
@@ -53,23 +44,18 @@ pixar_avalicao_publico |>
   mutate(filme = forcats::fct_reorder(filme, nota_rotten_tomatoes)) |>
   ggplot() +
   geom_point(aes(x = nota_rotten_tomatoes, y = filme, color = nota_rotten_tomatoes),
-             show.legend = FALSE) +
+             show.legend = FALSE) #+
   # a) ....
-  ggthemes::theme_solarized() +
   # b) ....
-  scale_color_viridis_c() +
   # c) ....
-  scale_x_continuous(limits = c(0, 100)) +
   # d)...
-  labs(x = "Nota", y = "Filme", caption = "Fonte: Rotten tomatoes - https://www.rottentomatoes.com/") +
   # e) ...
-  theme(axis.title = element_text(size = 10),
-        axis.text = element_text(size = 8))
+
 
 
 # 3. Usando o gráfico a seguir, faça os exercícios:
 # a) adicione o tema `theme_hc` usando o pacote ggthemes
-# b) altere a legenda do eixo x. dica: é uma  variável datetime.
+# b) altere a legenda do eixo x. Dica: é uma variável datetime.
 # configure os argumentos para que apareça as marcações de 
 # 3 em 3 meses, e para que os meses apareçam com o formato mes/ano, 
 # numérico. Ex: 03/13
@@ -81,11 +67,11 @@ pixar_avalicao_publico |>
 # e) com a função theme(): centralize o título do gráfico,
 # altere a posição da legenda para que ela fique no topo do gráfico 
 # (abaixo do título), altere os tamanhos da fonte, usando:
-# tamanho 12 para o título do gráfico
-# tamanho 9 para o título da legenda
-# tamanho 10 para os título dos eixos x e y
-# tamanho 8 para as legendas dos eixos x e y
-# Exemplo de resultado: 02-voos.png
+# - tamanho 12 para o título do gráfico
+# - tamanho 9 para o título da legenda
+# - tamanho 10 para os título dos eixos x e y
+# - tamanho 8 para as legendas dos eixos x e y
+# - Exemplo de resultado: 02-voos.png
 
 voos |>
   filter(companhia_aerea == "UA") |>
@@ -94,22 +80,9 @@ voos |>
   summarise(media_atraso_chegada = mean(atraso_chegada, na.rm = TRUE)) |>
   mutate(voo_atrasou = ifelse(media_atraso_chegada > 0 , "Sim",  "Não")) |>
   ggplot() +
-  geom_col(aes(x = dia_mes_ano, y = media_atraso_chegada, fill =  voo_atrasou)) +
+  geom_col(aes(x = dia_mes_ano, y = media_atraso_chegada, fill =  voo_atrasou)) #+
   # a) ....
-  ggthemes::theme_hc() +
   # b) ....
-  scale_x_datetime(date_breaks = "3 months", date_labels =  "%m/%y") +
   # c) ...
-  scale_fill_manual(values = c("#76A3E6", "#0030a4")) +
   # d) ...
-  labs(title = "Média de atraso na chegada do vôos da \n United Airlines em NYC",
-       x = "Mês/Ano",
-       y = "Média de atraso \n (em minutos)",
-       fill = "Em média, os vôos \natrasaram neste dia?") +
-  # e)
-  theme(legend.position = "top",
-        plot.title = element_text(hjust = 0.5, size = 12),
-        legend.title  = element_text(size = 9),
-        axis.title = element_text(size = 10),
-        axis.text = element_text(size = 8)
-        )
+  # e) ...
